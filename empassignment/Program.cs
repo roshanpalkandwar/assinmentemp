b8b46369ddd721a96850ded5569d947ef9a2de03
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-08-28 17:10:25 +0530

    [roshan]employee daily wages

--- empassignment/Program.cs
+++ empassignment/Program.cs
@@ -7,7 +7,7 @@ namespace empassignment
         static void Main(string[] args)
         {
             Console.WriteLine("welecom employee saraly masage");
-            Console.WriteLine("choose option\n 1.check absent present ");
+            Console.WriteLine("choose option\n 1.check absent present\n2.emp daily wages ");
             int opt = Convert.ToInt32(Console.ReadLine());
 
 
@@ -16,6 +16,9 @@ namespace empassignment
             {
                 case 1:
                     emppresenty.emplyeeattendcecheck();
+                    break;
+                 case 2:
+                    empdailywages.CalculateDilywage();
                     break;
 
             }
---
+++ empassignment/empdailywages.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace empassignment
+{
+    internal class empdailywages
+    {
+        public static void CalculateDilywage()
+        {
+
+            int empHrs = 0;
+            const int EMP_RATE_PER_HOUR = 20;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            Console.WriteLine("random genrate value " + empcheck);
+            if (empcheck == 0)
+
+            {
+                Console.WriteLine("emplyee present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("employee absent");
+                empHrs = 0;
+            }
+            int empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("daily emp wage is  " + empWage);
+        }
+    }
+}
