355f6ac2d52ef59a156fe1c19a933ef4443b2711
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-08-28 17:01:48 +0530

    [roshan]employee present absent

--- empassignment/Program.cs
+++ empassignment/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace empassignment
+﻿using System;
+
+namespace empassignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("welecom employee saraly masage");
+            Console.WriteLine("choose option\n 1.check absent present ");
+            int opt = Convert.ToInt32(Console.ReadLine());
+
+
+
+            switch (opt)
+            {
+                case 1:
+                    emppresenty.emplyeeattendcecheck();
+                    break;
+
+            }
         }
     }
 }
---
+++ empassignment/emppresenty.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace empassignment
+{
+    internal class emppresenty
+    {
+        public static void emplyeeattendcecheck()
+        {
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            Console.WriteLine("random genrate value " + empcheck);
+            if (empcheck == 0)
+            {
+                Console.WriteLine("emplyee present");
+
+            }
+            else
+            {
+                Console.WriteLine("employee absent");
+            }
+
+        }
+    }
+}
