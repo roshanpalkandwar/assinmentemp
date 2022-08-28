1f9fe01985151118d32a6df9b6ab4c30fc1f0051
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-08-28 16:21:59 +0530

    [roshan] employee welecome

---
+++ empassignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace empassignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welecom employee saraly masage");
+        }
+    }
+}
